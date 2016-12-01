4518ee6a76c09e7f42c5783d8b5637c10ca4365b
plainfingers <plainfingers@me.com>
2016-11-30 21:51:57 -0600

    Restructure layout

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+
 #----------------------------------------------------------------------------
 # bootstrap script to install core tools
 #----------------------------------------------------------------------------
 cd ~
 sudo pacman -S zsh grml-zsh-config linux-headers dialog wpa_supplicant openssh
+
+#----------------------------------------------------------------------------
+# desktop/x
+#----------------------------------------------------------------------------
+sudo pacman -S xorg-server xorg-server-utils xorg-xinit
+sudo pacman -S fvwm i3-wm i3status i3lock dmenu xdg-utils
+sudo pacman -S firefox
+sudo pacman -S ttf-bitstream-vera ttf-anonymous-pro ttf-hack
+sudo pacman -S pass              # cli password manager that is awesome
+sudo pacman -S task              # taskwarrior
+sudo pacman -S mutt abook aspell # email
+sudo pacman -S bluez bluez-utils
+
+#----------------------------------------------------------------------------
+# dev focused
+# - terminals
+# - virtualbox/vagrant
+#----------------------------------------------------------------------------
+sudo pacman -S xterm rxvt-unicode urxvt-perls weechat tmux
+sudo pacman -S virtualbox virtualbox-host-modules-arch
+# the following kernel modules need to be loaded with virtualbox
+modprobe vboxdrv vboxnetadp vboxnetflt vboxpci
+sudo pacman -S nfs-utils
+sudo pacman -S net-tools   # deprecated package, but still needed for virtualbox
+sudo pacman -S vagrant
+sudo pacman -S openvpn
 
 #----------------------------------------------------------------------------
 # vim/pathogen and deps
@@ -25,29 +52,6 @@ git clone https://github.com/tpope/vim-rails vim-rails
 git clone https://github.com/jgdavey/vim-blockle vim-blockle
 git clone https://github.com/vim-scripts/Align.git align
 cd ~
-
-#----------------------------------------------------------------------------
-# desktop/x
-#----------------------------------------------------------------------------
-sudo pacman -S xorg-server xorg-server-utils xorg-xinit
-sudo pacman -S xterm tmux rxvt-unicode urxvt-perls
-sudo pacman -S fvwm i3-wm i3status i3lock dmenu xdg-utils
-sudo pacman -S firefox
-sudo pacman -S ttf-bitstream-vera ttf-anonymous-pro ttf-hack
-sudo pacman -S pass              # cli password manager that is awesome
-sudo pacman -S task              # taskwarrior
-sudo pacman -S mutt abook aspell # email
-sudo pacman -S bluez bluez-utils
-
-#----------------------------------------------------------------------------
-# virtualbox/vagrant
-#----------------------------------------------------------------------------
-sudo pacman -S virtualbox virtualbox-host-modules-arch
-# the following kernel modules need to be loaded with virtualbox
-modprobe vboxdrv vboxnetadp vboxnetflt vboxpci
-sudo pacman -S nfs-utils
-sudo pacman -S net-tools   # deprecated package, but still needed for virtualbox
-sudo pacman -S vagrant
 
 #----------------------------------------------------------------------------
 # link dotfiles
